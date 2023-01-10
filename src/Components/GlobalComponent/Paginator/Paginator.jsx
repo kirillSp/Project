f978bac622de26f4485ff3486bd6fa30b2363388
Kirill <kirillspishukk@gmail.com>
2023-01-10 17:23:21 +0300

    upated files in folder Paginator

--- src/Components/GlobalComponent/Paginator/Paginator.jsx
+++ src/Components/GlobalComponent/Paginator/Paginator.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import StyleSelectPage from "./StyleSelectPage.module.css";
+import PaginatorS from "./PaginatorS.module.css";
 
-export let SelectPage = ({ totalUsers, pageSize, currentPage, clickPage, sectionSize = 5 }) => {
+export let Paginator = ({ totalUsers, pageSize, currentPage, clickPage, sectionSize = 5 }) => {
     let totalPages = Math.ceil(totalUsers / pageSize);
-    let pagesForRender = [];
+    let renderPages = [];
 
     for (let numPage = 1; numPage <= totalPages; numPage++) {
-        pagesForRender.push(numPage)
+        renderPages.push(numPage)
     }
 
     let totalSection = Math.ceil(totalPages / sectionSize);
@@ -14,18 +14,18 @@ export let SelectPage = ({ totalUsers, pageSize, currentPage, clickPage, section
     let firstPageSection = (currentSection - 1) * sectionSize + 1;
     let lastPageSection = currentSection * sectionSize;
 
-    return <div className={StyleSelectPage.numPage}>
+    return <div className={PaginatorS.numPage}>
         <>{
             currentSection > 1 && <button onClick={() => setSection(currentSection - 1)}>prev</button>
         }</>
 
         <>{
-            pagesForRender
-                .filter((numPage, i) => numPage >= firstPageSection && numPage <= lastPageSection)
+            renderPages
+                .filter(numPage => numPage >= firstPageSection && numPage <= lastPageSection)
                 .map((numPage, i) => {
-                    let isCurrentPage = (currentPage === numPage ? StyleSelectPage.selected : "") + " " + StyleSelectPage.pages;
+                    let cnVal = (currentPage === numPage ? PaginatorS.selected : "") + " " + PaginatorS.pages;
 
-                    return <span className={isCurrentPage} key={i} onClick={() => clickPage(numPage)}>{
+                    return <span className={cnVal} key={i} onClick={() => clickPage(numPage)}>{
                         numPage
                     }</span>
 
