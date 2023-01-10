9033164721c1afaed872f3d039ac2da6a7153904
Kirill <kirillspishukk@gmail.com>
2023-01-10 17:03:40 +0300

    update FindUsers file

--- src/Components/nav/FindUsers/FindUsers.jsx
+++ src/Components/nav/FindUsers/FindUsers.jsx
@@ -1,10 +1,10 @@
 import Preloader from "../../GlobalComponent/Preloader/Preloader";
 import { ListUsers } from "./ListUsers/ListUsers";
-import { SelectPage } from "../../GlobalComponent/SelectPage/SelectPage";
+import { Paginator } from "../../GlobalComponent/Paginator/Paginator";
 
 let FindUsers = props => {
     return <div>
-        <SelectPage totalUsers={props.totalUsers} pageSize={props.pageSize} currentPage={props.currentPage} clickPage={props.clickPage} />
+        <Paginator totalUsers={props.totalUsers} pageSize={props.pageSize} currentPage={props.currentPage} clickPage={props.clickPage} />
         <div>{props.isLoading ? <Preloader /> : null}</div>
         <ListUsers users={props.users} followingInProgress={props.followingInProgress} follow={props.follow} unfollow={props.unfollow} />
     </div>
